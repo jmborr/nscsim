c802140eea7f8a79dcf89eafef2e81a13406e968
Jose Borreguero <borreguero@gmail.com>
2018-08-22 11:12:50 -0400

    Refs #14 bunch of files to construct a PyPi package

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# nsc
-neutron scattering functions
+===
+nsc
+===
+
+
+.. image:: https://img.shields.io/pypi/v/nsc.svg
+        :target: https://pypi.python.org/pypi/nsc
+
+.. image:: https://travis-ci.org/jmborr/nsc.svg?branch=master
+        :target: https://travis-ci.org/jmborr/nsc/?badge=latest
+
+.. image:: https://readthedocs.org/projects/nsc/badge/?version=latest
+        :target: http://nsc.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+.. image:: https://badges.gitter.im/nsc/Lobby.svg
+   :alt: Join the chat at https://gitter.im/nsc/Lobby
+   :target: https://gitter.im/nsc/Lobby?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge
+
+Analysis of intrinsically disordered proteins by comparing MD simulations to
+Small Angle Scattering experiments.
---
+++ setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    'future', 'numpy', 'six',
+    # TODO: put package requirements here
+]
+
+setup_requirements = [
+    'pytest-runner',
+    # TODO(jmborr): put setup requirements (distutils extensions, etc.) here
+]
+
+test_requirements = [
+    'pytest',
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='nsc',
+    version='0.0.0',
+    long_description=readme,
+    author="Jose Borreguero",
+    author_email='borreguero@gmail.com',
+    url='https://github.com/jmborr/nsc',
+    packages=find_packages(include=['nsc']),
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords='nsc',
+    classifiers=[
+        'Development Status :: 0 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Physics',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+)
