3f08deb723918ed92809ee4640e60f1c4447b771
Jose Borreguero <borreguero@gmail.com>
2018-08-22 19:29:59 -0400

    Refs #16 refactor existing tests for qvec

--- tests/unit/test_qvec.py
+++ tests/unit/test_qvec.py
@@ -12,19 +12,16 @@ def test_unit_sphere_surf():
     assert sum(np.linalg.norm(v, axis=1)) == 42
 
 
-def test_q_mod_array_generator():
-    for qs in (qvec.moduli(0, q_mod_max=1.0, q_mod_delta=0.1),
-               qvec.moduli(0, q_mod_max=1.0, n_q_mod=10),
-               qvec.moduli(0, q_mod_delta=0.1, n_q_mod=10)):
+def test_moduli_linscale():
+    for qs in (qvec.moduli_linscale(0, q_mod_max=1.0, q_mod_delta=0.1),
+               qvec.moduli_linscale(0, q_mod_max=1.0, n_q_mod=10),
+               qvec.moduli_linscale(0, q_mod_delta=0.1, n_q_mod=10)):
         assert np.array_equal(qs, np.arange(0, 1.0, 0.1))
 
 
 def test_sphere_average():
-    for qs in (qvec.sphere_average(range(1, 10)),
-               qvec.sphere_average(q_mod_min=1, q_mod_max=10, q_mod_delta=1),
-               qvec.sphere_average(q_mod_min=1, q_mod_max=10, n_q_mod=9),
-               qvec.sphere_average(q_mod_min=1, q_mod_delta=1, n_q_mod=9)):
-        assert np.array_equal(9 * qs[0], qs[-1])
+    qs = qvec.sphere_average(range(1, 10))
+    assert np.array_equal(9 * qs[0], qs[-1])
 
 
 if __name__ == '__main__':
