88fbce74a454a3c652f6326a21108b27af8f95a0
Jose Borreguero <borreguero@gmail.com>
2019-05-14 12:13:21 -0400

    Refs #27 pytest.approx not liked by python 3.X

--- tests/unit/test_qvec.py
+++ tests/unit/test_qvec.py
@@ -4,8 +4,6 @@ import numpy as np
 from numpy.testing import assert_almost_equal, assert_array_equal
 from math import sqrt
 import pytest
-from pytest import approx
-
 from nscsim import qvec
 
 
@@ -20,7 +18,7 @@ def test_moduli_linscale():
                qvec.moduli_linscale(0, q_mod_delta=0.1, n_q_mod=10)):
         assert np.array_equal(qs, np.arange(0, 1.0, 0.1))
     qs = qvec.moduli_linscale(0, q_mod_max=1.0, n_q_mod=10)
-    assert qs == approx(np.linspace(0, 1, 10), rel=0.01)
+    assert_almost_equal(qs, np.linspace(0, 1, 10), decimal=2)
 
 
 def test_moduli_logscale():
