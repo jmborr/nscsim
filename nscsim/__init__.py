e766f220a1c39e64b967d4b887b9f518995d8480
Jose Borreguero <borreguero@gmail.com>
2019-05-14 17:51:34 -0400

    removed unnecessary imports

--- nscsim/__init__.py
+++ nscsim/__init__.py
@@ -1,10 +1,6 @@
 # flake8: noqa: F401
 
 """Top-level package for nscsim."""
-from __future__ import absolute_import
-
 __author__ = 'Jose Borreguero'
 __email__ = 'borreguero@gmail.com'
 __version__ = '0.0.0'
-
-from nscsim import qvec, elastic
